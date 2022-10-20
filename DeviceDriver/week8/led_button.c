449fea85a4dd84ab2759a004c37513c3fe3a9811
NewIntelligence4 <83831866+newintelligence4@users.noreply.github.com>
2022-10-20 12:27:18 +0900

    Create led_button.c

---
+++ DeviceDriver/week8/led_button.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+
+    char buff;
+    char tmp;
+    char prev = 'r';
+
+    int dev = open("/dev/my_gpio", O_RDWR);     // if you want read='O_RDONLY* write-'O_WRONLY', read&write='O_RDWR*
+    if(dev == -1) {
+        printf("Opening was not possible!\n");
+        return -1;
+    }
+
+    printf("Opening was successfull! \n");
+
+    while(1){
+        read(dev, &buff, 1);
+        prev = tmp;
+        tmp = buff;
+        write(dev, &tmp, 1);
+        if(prev != tmp)
+            printf("led is %c\n", tmp);
+    }
+    close (dev);
+    return 0;
+}
